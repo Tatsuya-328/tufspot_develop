ee56fce31dd8bce749be630c2ee59f458ff383f3
dumdum0727 <sekimen0727@gmail.com>
2023-09-16 15:02:53 +0900

    fix: articleページレスポンシブ途中

--- src/tufspot/resources/views/article_detail.blade.php
+++ src/tufspot/resources/views/article_detail.blade.php
@@ -5,11 +5,11 @@
     <x-bread />
     <x-main>
         <div class="article-detail-wrapper d-flex flex-column justify-content-center align-items-center">
-            <img src="{{ asset('image/article_detail.jpeg') }}" style="width:850px; height:500px;" class="" alt="...">
+            <img class="article-top-img" src="{{ asset('image/article_detail.jpeg') }}" class="" alt="...">
             {{-- <x-writer_card /> --}}
-            <div class="article-title d-flex justify-content-between ">
+            <div class="article-title">
                 <div class="article-title-text">ここにタイトルが入りますここにタイトルが入ります</div>
-                <div class="article-title-icon d-flex align-items-start">
+                <div class="article-title-icon align-items-start">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                         <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                     </svg>
