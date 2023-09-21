d6fb211b2b55b0235a2cd79c8df1c832fd83e1bf
Tatsuya-328 <kawanou328@yahoo.co.jp>
2023-09-21 23:54:16 +0900

    fix; 記事データ渡し未対応分をコメントアウト

--- src/tufspot/resources/views/writer_detail.blade.php
+++ src/tufspot/resources/views/writer_detail.blade.php
@@ -59,14 +59,15 @@
                     ▼<span>記事</span>
                 </p>
                 <div class="d-flex justify-content-center flex-wrap">
+                    {{-- TODO 記事表示 --}}
                     {{-- <div class="row row-cols-3"> --}}
-                    <x-article_card place="ハロン湾" />
+                    {{-- <x-article_card place="ハロン湾" />
                     <x-article_card place="スイティエン" />
-                    <x-article_card place="アンコールワット" />
+                    <x-article_card place="アンコールワット" /> --}}
                     {{-- 最終行も左寄せには、空要素入れるしかなさそう https://qiita.com/QUANON/items/e14949abab3711ca8646 --}}
-                    <x-article_card place="ハロン湾" />
+                    {{-- <x-article_card place="ハロン湾" />
                     <x-article_card place="スイティエン" />
-                    <x-article_card place="アンコールワット" />
+                    <x-article_card place="アンコールワット" /> --}}
                 </div>
             </div>
         </div>
