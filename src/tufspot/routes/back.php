37f6308d24dfea077366605e32d0edbb1e6a866e
Tatsuya-328 <kawanou328@yahoo.co.jp>
2023-09-22 11:45:34 +0900

    refactor: jsの読み込み箇所変更。エラー解消

--- src/tufspot/routes/back.php
+++ src/tufspot/routes/back.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Back\PostController;
 // adminのみアクセス可能
 Route::group(['middleware' => 'can:admin'], function () {
     // Route::get('/', 'DashboardController')->name('dashboard');
-    Route::get('/', [PostController::class, 'index'])->name('dashboard');
+    Route::get('/', [PostController::class, 'index'])->name('index');
     Route::resource('posts', 'PostController')->except('show');
     Route::resource('tags', 'TagController')->except('show');
     Route::resource('categories', 'CategoryController')->except('show');
