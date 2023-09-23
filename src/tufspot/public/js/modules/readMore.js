dbbd4d51aee303e09480d08fddf3390a2f27289b
dumdum0727 <sekimen0727@gmail.com>
2023-09-23 17:08:06 +0900

    fix: read moreの機能追加。記事が3列表示の時は３記事ずつ。２列と１列表示の時は２記事ずつ表示する。仮で「Academia」の部分の記事数を、JSで増やして挙動確認できるようにしてる。

--- src/tufspot/public/js/modules/readMore.js
+++ src/tufspot/public/js/modules/readMore.js
@@ -1,9 +1,12 @@
 'use strict';
 
 {
+    // 仮で記事数増やしてる
+    const articleContainers = document.querySelectorAll('.article-container');
     for (let i = 0; i < 15; i++) {
-        document.querySelector('.article-container').prepend(document.querySelector('.post_card'));
+        articleContainers[0].prepend(document.querySelector('.post_card'));
     }
+
     const featureWrappers = document.querySelectorAll('.top_feature_wrapper');
     const postCards1 = featureWrappers[0].querySelectorAll('.post_card');
     const postCards2 = featureWrappers[1].querySelectorAll('.post_card');
@@ -17,16 +20,76 @@
         postCard.classList.add('hidden');
     });
 
-    let startHiddenNumber = 6;
+    // read moreを押したら何個ずつ記事を表示するかの変数
+    let itemsPerClick = 6;
 
-    readMoreButtons[0].addEventListener('click', () => {
-        for (let i = 1; i <= 3; i++) {
-            if (postCards1[startHiddenNumber] === undefined) {
-                featureWrappers[0].querySelector('.blur-back').style.display = 'none';
-                return;
-            }
-            postCards1[startHiddenNumber].classList.remove('hidden');
-            startHiddenNumber++;
+     if (window.innerWidth <= 1199) {
+        itemsPerClick = 2;
+    } else {
+        itemsPerClick = 3;
+    }
+
+    // itemsPerClickを画面幅（列数）に応じて変更
+    window.addEventListener('resize', () => {
+        if (window.innerWidth <= 1199) {
+            itemsPerClick = 2;
+        } else {
+            itemsPerClick = 3;
         }
     });
+
+    {
+        let startHiddenNumber = 6;
+        if (postCards1[startHiddenNumber] === undefined) {
+            featureWrappers[0].querySelector('.blur-back').style.display = 'none';
+        } else {
+            readMoreButtons[0].addEventListener('click', () => {
+                for (let i = 1; i <= itemsPerClick; i++) {
+                    if (postCards1[startHiddenNumber] === undefined) {
+                        featureWrappers[0].querySelector('.blur-back').style.display = 'none';
+                        return;
+                    }
+                    postCards1[startHiddenNumber].classList.remove('hidden');
+                    startHiddenNumber++;
+                }
+            });
+        }
+
+    }
+    {
+        let startHiddenNumber = 6;
+        if (postCards2[startHiddenNumber] === undefined) {
+            featureWrappers[1].querySelector('.blur-back').style.display = 'none';
+        } else {
+            readMoreButtons[1].addEventListener('click', () => {
+                for (let i = 1; i <= itemsPerClick; i++) {
+                    if (postCards1[startHiddenNumber] === undefined) {
+                        featureWrappers[1].querySelector('.blur-back').style.display = 'none';
+                        return;
+                    }
+                    postCards2[startHiddenNumber].classList.remove('hidden');
+                    startHiddenNumber++;
+                }
+            });
+        }
+
+    }
+    {
+        let startHiddenNumber = 6;
+        if (postCards3[startHiddenNumber] === undefined) {
+            featureWrappers[2].querySelector('.blur-back').style.display = 'none';
+        } else {
+            readMoreButtons[2].addEventListener('click', () => {
+                for (let i = 1; i <= itemsPerClick; i++) {
+                    if (postCards1[startHiddenNumber] === undefined) {
+                        featureWrappers[2].querySelector('.blur-back').style.display = 'none';
+                        return;
+                    }
+                    postCards2[startHiddenNumber].classList.remove('hidden');
+                    startHiddenNumber++;
+                }
+            });
+        }
+
+    }
 }
