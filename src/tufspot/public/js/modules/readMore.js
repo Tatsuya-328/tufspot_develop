a792536f8931a97fe4e5b786a41f1c6c3cc73b15
dumdum0727 <sekimen0727@gmail.com>
2023-09-24 16:55:22 +0900

    fix: read moreの機能一通り完成。まだコードは冗長

--- src/tufspot/public/js/modules/readMore.js
+++ src/tufspot/public/js/modules/readMore.js
@@ -26,6 +26,7 @@ const readMoreButtons1 = articleContainers[0].querySelectorAll('.js-read-more');
 const readMoreButtons2 = articleContainers[1].querySelectorAll('.js-read-more');
 const readMoreButtons3 = articleContainers[2].querySelectorAll('.js-read-more');
 
+// Academiaのread more
 readMoreButtons1.forEach(readMoreButton => {
     readMoreButton.addEventListener('click', () => {
         if (window.innerWidth <= 425) {
@@ -56,7 +57,6 @@ readMoreButtons1.forEach(readMoreButton => {
 
                 articleContainers[0].querySelector('.blur-back').style.position = 'relative';
                 articleContainers[0].querySelector('.blur-back').style.height = '100px';
-                console.log(articleContainers[0].querySelector('.blur-back'))
             } else {
                 const targetElement = articleContainers[0].querySelector('.top_pickup_title_link');
                 const redirectLink = targetElement.getAttribute('href');
@@ -64,7 +64,88 @@ readMoreButtons1.forEach(readMoreButton => {
             }
         }
     });
-})
+});
+
+// Businessのread more
+readMoreButtons2.forEach(readMoreButton => {
+    readMoreButton.addEventListener('click', () => {
+        if (window.innerWidth <= 425) {
+            const targetElement = articleContainers[1].querySelector('.top_pickup_title_link');
+            const redirectLink = targetElement.getAttribute('href');
+            window.location.href = redirectLink;
+        } else if (window.innerWidth <= 1199) {
+            let removeHiddenCount = 0;
+            let hasHiddenElement = false;
+            postCards2.forEach(postCard => {
+                if (postCard.classList.contains('hidden') && removeHiddenCount <= 3) {
+                    postCard.classList.remove('hidden');
+                    removeHiddenCount++;
+                    hasHiddenElement = true;
+                }
+            });
+
+            if (hasHiddenElement === false) {
+                const targetElement = articleContainers[1].querySelector('.top_pickup_title_link');
+                const redirectLink = targetElement.getAttribute('href');
+                window.location.href = redirectLink;
+            }
+        } else {
+            if (postCards2[postCards2.length - 1].classList.contains('hidden')) {
+                postCards2.forEach(postCard => {
+                    postCard.classList.remove('hidden');
+                });
+
+                articleContainers[1].querySelector('.blur-back').style.position = 'relative';
+                articleContainers[1].querySelector('.blur-back').style.height = '100px';
+            } else {
+                const targetElement = articleContainers[1].querySelector('.top_pickup_title_link');
+                const redirectLink = targetElement.getAttribute('href');
+                window.location.href = redirectLink;
+            }
+        }
+    });
+});
+
+// Culture and Essayのread more
+readMoreButtons3.forEach(readMoreButton => {
+
+    readMoreButton.addEventListener('click', () => {
+        if (window.innerWidth <= 425) {
+            const targetElement = articleContainers[2].querySelector('.top_pickup_title_link');
+            const redirectLink = targetElement.getAttribute('href');
+            window.location.href = redirectLink;
+        } else if (window.innerWidth <= 1199) {
+            let removeHiddenCount = 0;
+            let hasHiddenElement = false;
+            postCards3.forEach(postCard => {
+                if (postCard.classList.contains('hidden') && removeHiddenCount <= 3) {
+                    postCard.classList.remove('hidden');
+                    removeHiddenCount++;
+                    hasHiddenElement = true;
+                }
+            });
+
+            if (hasHiddenElement === false) {
+                const targetElement = articleContainers[2].querySelector('.top_pickup_title_link');
+                const redirectLink = targetElement.getAttribute('href');
+                window.location.href = redirectLink;
+            }
+        } else {
+            if (postCards3[postCards3.length - 1].classList.contains('hidden')) {
+                postCards3.forEach(postCard => {
+                    postCard.classList.remove('hidden');
+                });
+
+                articleContainers[2].querySelector('.blur-back').style.position = 'relative';
+                articleContainers[2].querySelector('.blur-back').style.height = '100px';
+            } else {
+                const targetElement = articleContainers[2].querySelector('.top_pickup_title_link');
+                const redirectLink = targetElement.getAttribute('href');
+                window.location.href = redirectLink;
+            }
+        }
+    });
+});
 
 function addHiddenClass(postCards) {
     if (window.innerWidth <= 425) {
@@ -96,114 +177,3 @@ function addHiddenClass(postCards) {
         });
     }
 }
-
-// // 仮で記事数増やしてる
-// const articleContainers = document.querySelectorAll('.article-container');
-// for (let i = 0; i < 15; i++) {
-//     articleContainers[0].prepend(document.querySelector('.post_card'));
-// }
-
-// const featureWrappers = document.querySelectorAll('.top_feature_wrapper');
-// const postCards1 = featureWrappers[0].querySelectorAll('.post_card');
-// const postCards2 = featureWrappers[1].querySelectorAll('.post_card');
-// const postCards3 = featureWrappers[2].querySelectorAll('.post_card');
-// let readMoreButtons = document.querySelectorAll('.read-more-button');
-// let itemsPerClick = 3; // read moreを押したら何個ずつ記事を表示するかの変数
-
-// addHiddenClass();
-
-// window.addEventListener('resize', () => {
-//     addHiddenClass();
-// });
-
-// // itemsPerClickを画面幅（列数）に応じて変更
-// window.innerWidth <= 1199 ? itemsPerClick = 2 : itemsPerClick = 3;
-// window.addEventListener('resize', () => {
-//     window.innerWidth <= 1199 ? itemsPerClick = 2 : itemsPerClick = 3;
-// });
-
-// {
-//     let startHiddenNumber = 6;
-//     const readMore = new ReadMore(featureWrappers[0], readMoreButtons[0], postCards1);
-//     readMore.readMoreClick(itemsPerClick, startHiddenNumber);
-// }
-// {
-//     let startHiddenNumber = 6;
-//     if (postCards2[startHiddenNumber] === undefined) {
-//         featureWrappers[1].querySelector('.blur-back').style.display = 'none';
-//     } else {
-//         readMoreButtons[1].addEventListener('click', () => {
-//             for (let i = 1; i <= itemsPerClick; i++) {
-//                 if (postCards1[startHiddenNumber] === undefined) {
-//                     featureWrappers[1].querySelector('.blur-back').style.display = 'none';
-//                     return;
-//                 }
-//                 postCards2[startHiddenNumber].classList.remove('hidden');
-//                 startHiddenNumber++;
-//             }
-//         });
-//     }
-
-// }
-// {
-//     let startHiddenNumber = 6;
-//     if (postCards3[startHiddenNumber] === undefined) {
-//         featureWrappers[2].querySelector('.blur-back').style.display = 'none';
-//     } else {
-//         readMoreButtons[2].addEventListener('click', () => {
-//             for (let i = 1; i <= itemsPerClick; i++) {
-//                 if (postCards1[startHiddenNumber] === undefined) {
-//                     featureWrappers[2].querySelector('.blur-back').style.display = 'none';
-//                     return;
-//                 }
-//                 postCards2[startHiddenNumber].classList.remove('hidden');
-//                 startHiddenNumber++;
-//             }
-//         });
-//     }
-
-// }
-
-// // PC画面幅の時はデフォ6記事表示。SP画面幅の時はデフォ4記事表示する関数
-// function addHiddenClass() {
-//     postCards1.forEach((postCard, index) => {
-//         if (window.innerWidth <= 1199) {
-//             if (index <= 3) {
-//                 postCard.classList.remove('hidden');
-//                 return;
-//             }
-//         } else {
-//             if (index <= 5) {
-//                 postCard.classList.remove('hidden');
-//                 return;
-//             };
-//         }
-
-//         postCard.classList.add('hidden');
-//     });
-// }
-
-// class ReadMore {
-//     constructor(featureWrapper, readMoreButton, postCards) {
-//         this.featureWrapper = featureWrapper;
-//         this.readMoreButton = readMoreButton;
-//         this.postCard = postCards;
-//     }
-
-//     readMoreClick(itemsPerClick, startHiddenNumber) {
-//         if (this.postCards[startHiddenNumber] === undefined) {
-//             this.featureWrapper.querySelector('.blur-back').style.display = 'none';
-//         } else {
-//             this.readMoreButton.addEventListener('click', () => {
-//                 for (let i = 1; i <= itemsPerClick; i++) {
-//                     if (this.postCards[startHiddenNumber] === undefined) {
-//                         this.featureWrapper.querySelector('.blur-back').style.display = 'none';
-//                         return;
-//                     }
-//                     this.postCards[startHiddenNumber].classList.remove('hidden');
-//                     this.startHiddenNumber++;
-//                 }
-//             });
-//         }
-//     }
-// }
