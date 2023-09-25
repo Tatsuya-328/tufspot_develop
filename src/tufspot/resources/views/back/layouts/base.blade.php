dd1b23a441276f5a858374dfd4b235739583a9b7
Tatsuya-328 <kawanou328@yahoo.co.jp>
2023-09-25 22:09:16 +0900

    adminテンプレ 色や感覚調整 サイドバー開閉

--- src/tufspot/resources/views/back/layouts/base.blade.php
+++ src/tufspot/resources/views/back/layouts/base.blade.php
@@ -56,12 +56,12 @@
 
 <body class="vsc-initialized">
 
-    <header class="navbar navbar-dark  bg-dark flex-md-nowrap p-0 shadow fixed-top">
+    {{-- <header class="navbar navbar-light  bg-light flex-md-nowrap p-0 shadow fixed-top">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{ route('back.index') }}">TUFSPOT管理</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler d-md-none mb-3 collapsed" type="button" data-toggle="collapse" data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+        <input class="form-control form-control-light w-100" type="text" placeholder="Search" aria-label="Search">
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
                 <a class="nav-link" href="{{ route('back.users.edit', Auth::user()['id']) }}">{{ Auth::user()['name'] }}</a>
@@ -72,12 +72,40 @@
                 <a class="nav-link" href="{{ route('logout') }}">Logout</a>
             </li>
         </ul>
-    </header>
+    </header> --}}
 
+    <nav class="navbar navbar-light bg-light p-3 fixed-top">
+        <div class="d-flex col-12 col-md-3 col-lg-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
+            <a class="navbar-brand" href="{{ route('back.index') }}">
+                TUFSPOT管理
+            </a>
+            <button class="navbar-toggler d-md-none mb-1 collapsed" type="button" data-toggle="collapse" data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        {{-- <div class="col-12 col-md-4 col-lg-2">
+            <input class="form-control form-control-dark" type="text" placeholder="Search" aria-label="Search">
+        </div> --}}
+        <div class="col-12 col-md-5 col-lg-8 d-flex align-items-center justify-content-md-end mt-md-0">
+            <div class="mr-3 mt-1">
+                <span></span>
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+                    {{ Auth::user()['name'] }}さん
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    {{-- <li><a class="dropdown-item" href="#">Settings</a></li> --}}
+                    <li><a class="dropdown-item" href="{{ route('back.users.edit', Auth::user()['id']) }}">登録情報</a></li>
+                    <li><a class="dropdown-item" href={{ route('logout') }}>Log out</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container-fluid">
         <div class="row">
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                <div class="position-sticky pt-3">
+            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky">
                     <ul class="nav flex-column">
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('back.posts.index') }}">
@@ -138,7 +166,7 @@
                     </ul>
                 </div>
             </nav>
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 mt-5">
+            <main class="admin-main col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-md-12">
@@ -170,6 +198,12 @@
             </svg></div>
         <div id="yt_post_summary_close_button" class="yt_post_summary_close_button">×</div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
     {{-- Quill editor --}}
