3a9c4d57d2ab7a10700a551677d09d7b46383b4d
Tatsuya-328 <kawanou328@yahoo.co.jp>
2023-09-28 15:58:01 +0900

    fix 投稿リストテーブル レスポンシブ

--- src/tufspot/resources/views/back/posts/index.blade.php
+++ src/tufspot/resources/views/back/posts/index.blade.php
@@ -75,90 +75,92 @@ $title = '記事一覧';
 
         @if (0 < $posts->count())
             {{-- <table class="table table-striped table-bordered table-hover table-sm"> --}}
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">タイトル</th>
-                        <th scope="col" style="width: 5em">状態</th>
-                        <th scope="col" style="width: 10em">カテゴリー</th>
-                        <th scope="col" style="width: 10em">特集項目</th>
-                        <th scope="col" style="width: 10em">公開日</th>
-                        <th scope="col" style="width: 10em">執筆者</th>
-                        <th scope="col" style="width: 5em"></th>
-                        <th scope="col" style=""></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($posts as $post)
+            <div class="table-responsive">
+                <table class="table ">
+                    <thead>
                         <tr>
-                            <td>{{ $post->id }}</td>
-                            <td>{{ $post->title }}</td>
-                            <td>{{ $post->is_public_label }}</td>
-                            <td>
-                                @foreach ($post->categories as $category)
-                                    @if (!$loop->first)
-                                        、
-                                    @endif
-                                    {{ $category->name }}
-                                @endforeach
-                            </td>
-                            <td>
-                                @foreach ($post->features as $feature)
-                                    @if (!$loop->first)
-                                        、
-                                    @endif
-                                    {{ $feature->name }}
-                                @endforeach
-                            </td>
-                            <td>{{ $post->published_format }}</td>
-                            <td>{{ $post->user->name }}</td>
-                            <td>
-                                {{-- {{ link_to_route('post_detail', '本番確認', $post, [
+                            <th scope="col" class="text-nowrap">ID</th>
+                            <th scope="col" style="width: 15em" class="text-nowrap">タイトル</th>
+                            <th scope="col" style="width: 5em" class="text-nowrap">状態</th>
+                            <th scope="col" style="width: 10em" class="text-nowrap">カテゴリー</th>
+                            <th scope="col" style="width: 10em" class="text-nowrap">特集項目</th>
+                            <th scope="col" style="width: 10em" class="text-nowrap">公開日</th>
+                            <th scope="col" style="width: 10em" class="text-nowrap">執筆者</th>
+                            <th scope="col" style="width: 5em" class="text-nowrap"></th>
+                            <th scope="col" style=""></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td>{{ $post->id }}</td>
+                                <td>{{ $post->title }}</td>
+                                <td>{{ $post->is_public_label }}</td>
+                                <td>
+                                    @foreach ($post->categories as $category)
+                                        @if (!$loop->first)
+                                            、
+                                        @endif
+                                        {{ $category->name }}
+                                    @endforeach
+                                </td>
+                                <td>
+                                    @foreach ($post->features as $feature)
+                                        @if (!$loop->first)
+                                            、
+                                        @endif
+                                        {{ $feature->name }}
+                                    @endforeach
+                                </td>
+                                <td>{{ $post->published_format }}</td>
+                                <td>{{ $post->user->name }}</td>
+                                <td>
+                                    {{-- {{ link_to_route('post_detail', '本番確認', $post, [
                                     'class' => 'btn btn-outline-dark btn-sm m-1',
                                     'target' => '_blank',
                                 ]) }} --}}
-                                {{-- {{ link_to_route('index', '詳細', $post, [
+                                    {{-- {{ link_to_route('index', '詳細', $post, [
                                     'class' => 'btn btn-secondary btn-sm m-1',
                                     'target' => '_blank',
                                 ]) }} --}}
-                                {{ link_to_route('back.posts.edit', '編集', $post, [
-                                    'class' => 'btn btn-outline-dark btn-sm m-1',
-                                ]) }}
-                            </td>
-                            <td>
-                                <div class="dropdown">
-                                    <a class="text-decoration-none link-dark pb-3" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-                                            <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
-                                        </svg>
-                                    </a>
-                                    {{-- <button class="btn btn-secondary" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {{ link_to_route('back.posts.edit', '編集', $post, [
+                                        'class' => 'btn btn-outline-dark btn-sm m-1',
+                                    ]) }}
+                                </td>
+                                <td>
+                                    <div class="dropdown">
+                                        <a class="text-decoration-none link-dark pb-3" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                                            </svg>
+                                        </a>
+                                        {{-- <button class="btn btn-secondary" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
                                             <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
                                         </svg>
                                     </button> --}}
-                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                        {{-- <li><a class="dropdown-item" href="#">Action</a></li> --}}
-                                        <li><a class="dropdown-item" href="{{ route('post_detail', [$post]) }}">本番確認</a></li>
-                                        <li>
-                                            {{ Form::model($post, [
-                                                'route' => ['back.posts.destroy', $post],
-                                                'method' => 'delete',
-                                            ]) }}
-                                            {{ Form::submit('削除', [
-                                                'onclick' => "return confirm('本当に削除しますか?')",
-                                                'class' => 'dropdown-item text-danger',
-                                            ]) }}
-                                            {{ Form::close() }}
-                                        </li>
-                                    </ul>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                            {{-- <li><a class="dropdown-item" href="#">Action</a></li> --}}
+                                            <li><a class="dropdown-item" href="{{ route('post_detail', [$post]) }}">本番確認</a></li>
+                                            <li>
+                                                {{ Form::model($post, [
+                                                    'route' => ['back.posts.destroy', $post],
+                                                    'method' => 'delete',
+                                                ]) }}
+                                                {{ Form::submit('削除', [
+                                                    'onclick' => "return confirm('本当に削除しますか?')",
+                                                    'class' => 'dropdown-item text-danger',
+                                                ]) }}
+                                                {{ Form::close() }}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
             <div class="d-flex justify-content-center">
                 <nav aria-label="Page navigation example">
                     <ul class="pagination">
