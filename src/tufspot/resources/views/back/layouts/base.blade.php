70c27c0fc0fc6664903206b4d21aa8ae1f518ea0
Tatsuya-328 <kawanou328@yahoo.co.jp>
2023-09-30 17:31:48 +0900

    fix: adminヘッダの別タブオープンをロゴから本番に修正

--- src/tufspot/resources/views/back/layouts/base.blade.php
+++ src/tufspot/resources/views/back/layouts/base.blade.php
@@ -75,14 +75,14 @@
     </header> --}}
     <nav class="navbar navbar-expand-lg navbar-light bg-light ">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('back.index') }}" target="_blank" rel="noopener noreferrer">TUFSPOT管理</a>
+            <a class="navbar-brand" href="{{ route('back.index') }}">TUFSPOT管理</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('index') }}">本番サイト</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('index') }}" target="_blank" rel="noopener noreferrer">本番サイト</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('back.posts.index') }}">記事</a>
