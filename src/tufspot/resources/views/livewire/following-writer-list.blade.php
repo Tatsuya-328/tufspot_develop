819cc2632444e9f029711577946ffbca56d1474a
Tatsuya-328 <kawanou328@yahoo.co.jp>
2023-10-02 11:53:49 +0900

    feat: マイページで記事やフォローライターいなかった時の文言追加

--- src/tufspot/resources/views/livewire/following-writer-list.blade.php
+++ src/tufspot/resources/views/livewire/following-writer-list.blade.php
@@ -1,8 +1,12 @@
 <div x-data @page-updated.window="$el.scrollIntoView({behavior: 'auto'})">
     <div class="d-flex justify-content-center flex-wrap">
-        @foreach ($following_writers as $writer)
-            <x-writer_card :writer=$writer />
-        @endforeach
+        @if (!empty($following_writers->first()))
+            @foreach ($following_writers as $writer)
+                <x-writer_card :writer=$writer />
+            @endforeach
+        @else
+            <p class="text-center m-5 p-5">フォロー済みライターが存在しません。</p>
+        @endif
     </div>
     <div class="d-flex justify-content-center">
         {{ $following_writers->links() }}
