af2d3a820a6927e5b70c121a5f2131d3473b455a
kk09bass <kk09bass@gmail.com>
2023-10-12 22:41:26 +0900

    fix: カテゴリー/特集記事ページのタイトル部の余白を最適化

--- src/tufspot/resources/views/components/post_list_title.blade.php
+++ src/tufspot/resources/views/components/post_list_title.blade.php
@@ -2,7 +2,7 @@
 
 <div class="post_list_title {{ $class }}">
     <x-bread />
-    <div class="post_list_title_content">
+    <div class="container post_list_title_content">
         <p class="post_list_title_text m-0">{{ $listTitle }}</p>
     </div>
 </div>
