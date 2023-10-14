903dec89d6390cf604341debbb2270640853eee9
kk09bass <kk09bass@gmail.com>
2023-10-14 17:35:29 +0900

    fix: 検索結果画面の検索ワードのレイアウトを調整

--- src/tufspot/resources/views/search_result.blade.php
+++ src/tufspot/resources/views/search_result.blade.php
@@ -5,15 +5,17 @@
     <x-post_list_title class="unset-shadow" listTitle="検索結果" />
     <x-main>
         {{-- TODO: 検索ワードのスタイリングはいったんカテゴリーからとってきてる、要修正 --}}
-        <div class="post_list_explain d-flex flex-column justify-content-center">
-            <p>
-                検索対象：{{ config('common.search_filter')[$search_filter] }}
+        <div class="post_list_explain d-flex flex-column flex-md-row justify-content-around">
+            <p class="text-center">
+                検索範囲：
+                [{{ config('common.search_filter')[$search_filter] }}]
             </p>
-            @foreach ($keywordArr as $keyword)
-                <p class="post_list_explain_text m-0">
-                    {{ $keyword }}
-                </p>
-            @endforeach
+            <p class="text-center">
+                検索ワード：
+                @foreach ($keywordArr as $keyword)
+                    "{{ $keyword }}"
+                @endforeach
+            </p>
         </div>
         <div class="article-list-area">
             <livewire:paginated-post-list :keywords="$keywordArr" :search_filter="$search_filter" page_flag="search" />
