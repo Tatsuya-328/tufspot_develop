99f2df724a3fd1ff58159c319cf16cccbe20c5c5
oct216 <oct216@blubla.net>
2023-10-27 08:34:34 +0900

    feat: writer 画面で画像設定済みなら画像を、無ければキツネのイラストを表示

--- src/tufspot/resources/views/components/writer_card.blade.php
+++ src/tufspot/resources/views/components/writer_card.blade.php
@@ -1,8 +1,7 @@
 <div class="writer-card d-flex flex-wrap flex-column align-content-center" wire:key="writer-{{ $writer->id }}">
     <a href="{{ route('writer_detail', ['user' => $writer['id']]) }}" class="text-decoration-none">
         <div class="d-flex flex-wrap flex-column justify-content-center align-content-center">
-            {{-- TODO: アイコン画像表示 --}}
-            <img loading="lazy" src="{{ asset('image/fox_circle.png') }}" style="width:100px; height:100px;" class="" alt="...">
+            <img loading="lazy" src="{{ $writer['profile_image_path'] ?? asset('image/fox_circle.png') }}" style="width: 26px; height: 26px;" class="" alt="{{ $writer['name'] }} のプロフィール画像" />
         </div>
         <p class="writer-card-name text-center">
             {{ $writer['name'] }}
